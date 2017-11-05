4502275201bf7a0118eccf577cd778445b8e0d01
lanto <lanto@lantodeMacBook.local>
2017-11-05 22:19:10 +0800

    add

--- src/page/register.vue
+++ src/page/register.vue
@@ -6,7 +6,7 @@
     <div class="input">
       <mt-field label="手机号" placeholder="手机号" v-model="tel"></mt-field>
       <mt-field label="验证码" placeholder="验证码" v-model="code">
-        <span>获取验证码</span>
+        <span @click="getCode">{{getCodeButton}}</span>
       </mt-field>
       <mt-field label="密码" placeholder="输入密码" type="password" v-model="password"></mt-field>
       <mt-field label="确认密码" placeholder="再次输入密码" type="password" v-model="rpassword"></mt-field>
@@ -24,17 +24,20 @@
         :options="['同意《上海汽修平台协议》']">
       </mt-checklist>
     </div>
-    <mt-button type="primary">注册</mt-button>
+    <mt-button type="primary" @click="register">注册</mt-button>
   </div>
 </template>
 
 <script>
+  import { Toast } from 'mint-ui';
+  import { MessageBox } from 'mint-ui';
   export default {
     name: 'register',
     data () {
       return {
         tel:"",
         code:"",
+        getCodeButton:"获取验证码",
         password:"",
         rpassword:"",
         radiov: "",
@@ -43,12 +46,115 @@
           {label: '企业注册', value: '2'},
           {label: '管理注册', value: '3'}
         ],
-        agree:[]
+        agree:[],
+        count: 0,
+        timer: null
       }
     },
     methods:{
       goback(){
         this.$router.go(-1)
+      },
+      getCode(){
+        if( !/^13[0-9]{9}$|14[0-9]{9}$|15[0-9]{9}$|17[0-9]{9}$|18[0-9]{9}$/.test(this.tel) ){
+          Toast('手机号不正确！');
+          return
+        }
+        if(this.timer) return;
+
+        let data={
+          access_token: localStorage.getItem('SYSTEMTOKEN'),
+          usertel: this.tel
+        }
+        this.axios({
+          method: 'post',
+          url: '/message/sms/sendcaptcha',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          data: JSON.stringify(data)
+        }).then(function (response) {
+          console.log(response)
+          if(response.data.code=='000000'){
+            Toast('发送成功，请查收！');
+          }else Toast(response.data.status);
+
+        })
+
+
+        //计时器
+        const TIME_COUNT = 60;
+        let num= 0;
+        if (!this.timer) {
+          this.count = TIME_COUNT;
+
+          this.timer = setInterval(() => {
+            if (this.count > 0 && this.count <= TIME_COUNT) {
+              num= this.count
+              this.getCodeButton= num+ "s"
+              this.count--;
+            } else {
+              clearInterval(this.timer);
+              this.getCodeButton= '获取验证码'
+              this.timer = null;
+            }
+          }, 1000)
+        }
+      },
+      register(){
+        if( !/^13[0-9]{9}$|14[0-9]{9}$|15[0-9]{9}$|17[0-9]{9}$|18[0-9]{9}$/.test(this.tel) ){
+          Toast('手机号不正确！');
+          return
+        }
+        if(! /(.+){6,18}$/.test(this.password)){
+          Toast('密码必须6到18位！');
+          return
+        }
+        if(this.password!= this.rpassword){
+          Toast('两次密码不一致！');
+          return
+        }
+        if(!this.code){
+          Toast('请输入验证码！');
+          return
+        }
+        if(!this.radiov){
+          Toast('请选择注册类型（注册后不可修改）！');
+          return
+        }
+        if(this.agree.length==0){
+          Toast('请同意《上海汽修平台协议》');
+          return
+        }
+
+        let self= this
+        let data={
+          "captcha": this.code,
+          "systemToken": localStorage.getItem('SYSTEMTOKEN'),
+          "useroleid": this.radiov,
+          "userpassword": this.password,
+          "usertel": this.tel
+        }
+        this.axios({
+          method: 'post',
+          url: '/user/useraccount/register',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          data: JSON.stringify(data)
+        }).then(function (response) {
+          console.log(response)
+          if(response.data.code=='000000'){
+            MessageBox.alert('注册成功').then(action => {
+              self.$router.push({
+                path:'/login'
+              })
+            });
+          }else {
+            MessageBox('提示', response.data.status);
+          }
+
+        })
       }
     }
   }
